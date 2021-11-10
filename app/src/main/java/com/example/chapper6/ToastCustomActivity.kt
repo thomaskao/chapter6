25cc27294cc4b0ce9ee41ca91189fa845eca8467
thomaskao <thomaskao99@gmail.com>
2021-11-10 20:09:15 +0800

    first

---
+++ app/src/main/java/com/example/chapper6/ToastCustomActivity.kt
@@ -0,0 +1,11 @@
+package com.example.chapper6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ToastCustomActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.toast_custom)
+    }
+}
